d9ee897a799b0a836428d9cdf6780a3a830a1140
kotahorii <khorii.micron@gmail.com>
2021-12-28 16:58:23 +0900

    [Add] post data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(
-  email: "user1@gmail.com",
+  email: "user1@test.com",
   password: 'aaaaaa',
   name: '高橋 健',
   image: open('./app/images/user/user1.jpeg'),
@@ -8,7 +8,7 @@ User.create!(
 )
 
 User.create!(
-  email: "user2@gmail.com",
+  email: "user2@test.com",
   password: 'aaaaaa',
   name: '太田 誠',
   image: open('./app/images/user/user2.jpeg'),
@@ -17,7 +17,7 @@ User.create!(
 )
 
 User.create!(
-  email: "user3@gmail.com",
+  email: "user3@test.com",
   password: 'aaaaaa',
   name: '田中 一夫',
   image: open('./app/images/user/user3.jpeg'),
@@ -26,7 +26,7 @@ User.create!(
 )
 
 User.create!(
-  email: "user4@gmail.com",
+  email: "user4@test.com",
   password: 'aaaaaa',
   name: '岡田 裕之',
   image: open('./app/images/user/user4.jpeg'),
@@ -35,7 +35,7 @@ User.create!(
 )
 
 User.create!(
-  email: "user5@gmail.com",
+  email: "user5@test.com",
   password: 'aaaaaa',
   name: '田中 歩',
   image: open('./app/images/user/user5.jpeg'),
@@ -44,7 +44,7 @@ User.create!(
 )
 
 User.create!(
-  email: "user6@gmail.com",
+  email: "user6@test.com",
   password: 'aaaaaa',
   name: '佐藤 一郎',
   image: open('./app/images/user/user6.jpeg'),
@@ -63,26 +63,14 @@ User.create!(
 
 Post.create!(
   user_id: 4,
-  title: '五稜郭タワー',
-  body: '歴史を感じさせます。',
+  title: '札幌駅',
+  body: '夜景が綺麗です',
   prefecture: '北海道',
-  city: '函館市',
-  town: '五稜郭町',
-  image: open('./app/images/post/goryokaku.jpeg'),
-  lat: 41.795413,
-  lng: 140.752721,
-)
-
-Post.create!(
-  user_id: 4,
-  title: 'さっぽろテレビ塔',
-  body: 'すごく大きいです。',
-  prefecture: '北海道',
-  city: '札幌市中央区',
-  town: '大通西',
-  image: open('./app/images/post/sapporo_tower.jpeg'),
-  lat: 43.05934,
-  lng: 141.34198,
+  city: '虻田郡洞爺湖町',
+  town: '洞爺町',
+  image: open('./app/images/post/toyako.jpeg'),
+  lat: 42.654391,
+  lng: 140.825134,
 )
 
 Post.create!(
@@ -95,6 +83,18 @@ Post.create!(
   image: open('./app/images/post/kamuikotan.jpeg'),
   lat: 43.721547,
   lng: 142.198833,
+)
+
+Post.create!(
+  user_id: 4,
+  title: 'さっぽろテレビ塔',
+  body: 'すごく大きいです。',
+  prefecture: '北海道',
+  city: '札幌市中央区',
+  town: '大通西',
+  image: open('./app/images/post/sapporo_tower.jpeg'),
+  lat: 43.05934,
+  lng: 141.34198,
 )
 
 Post.create!(
@@ -119,4 +119,16 @@ Post.create!(
   image: open('./app/images/post/tsutenkaku.jpeg'),
   lat: 34.651238,
   lng: 135.50575,
+)
+
+Post.create!(
+  user_id: 1,
+  title: '天王寺公園',
+  body: '池が綺麗な公園です',
+  prefecture: '大阪府',
+  city: '大阪市天王寺区',
+  town: '茶臼山町',
+  image: open('./app/images/post/tennnoji_park.jpeg'),
+  lat: 34.650324,
+  lng: 135.512232,
 )
